e024ad5b70cbc583610bbbd09044ba77d25dfbb1
franfran <fgg67@hotmail.fr>
2023-05-17 12:29:49 +0300

    initial

---
+++ src/main.rs
@@ -0,0 +1,108 @@
+enum Format {
+    Naive,
+    Advanced(Params),
+}
+
+#[derive(Debug)]
+struct Params {
+    max_width: usize,
+}
+
+fn main() {
+    let text = std::env::args().nth(1).expect("Usage: <binary> TEXT");
+    let formatted = format_k(text, Format::Naive);
+    println!("{}", formatted);
+}
+
+fn format_k(text: String, format: Format) -> String {
+    match format {
+        Format::Naive => naive_format(text),
+        Format::Advanced(_) => unimplemented!(),
+    }
+}
+
+fn naive_format(text: String) -> String {
+    let mut out = String::from("");
+
+    let mut level = 0;
+
+    for c in text.chars() {
+        if c == '(' {
+            out.push(c);
+            level += 1;
+            out.push('\n');
+            // (0..level).for_each(|_| out.push('\t'));
+            (0..level).for_each(|_| out.push_str("      "));
+
+            continue;
+        } else if c == ')' {
+            level -= 1;
+            out.push('\n');
+            (0..level).for_each(|_| out.push_str("      "));
+        }
+
+        out.push(c);
+    }
+
+    out
+}
+
+#[test]
+fn smol_naive() {
+    let text = "(KApply(),)".to_string();
+    let formatted = format_k(text, Format::Naive);
+
+    let expected = "(
+    KApply(
+        
+    ),
+)"
+    .to_string();
+
+    assert_eq!(expected, formatted);
+}
+
+#[test]
+fn format() {
+    let text = "(KApply(label=KLabel(name='#Equals', params=(KSort(name='Bool'), KSort(name='GeneratedTopCell'))), args=(KToken(token='true', sort=KSort(name='Bool')), KApply(label=KLabel(name='foundry_success', params=()), args=(KVariable(name='STATUSCODE_FINAL', sort=KSort(name='StatusCode')), KApply(label=KLabel(name='#lookup(_,_)_EVM-TYPES_Int_Map_Int', params=()), args=(KVariable(name='CHEATCODE_STORAGE_FINAL', sort=KSort(name='Map')), KToken(token='46308022326495007027972728677917914892729792999299745830475596687180801507328', sort=KSort(name='Int')))), KVariable(name='ISREVERTEXPECTED_FINAL', sort=KSort(name='Bool')), KVariable(name='ISOPCODEEXPECTED_FINAL', sort=KSort(name='Bool')), KVariable(name='RECORDEVENT_FINAL', sort=KSort(name='Bool')), KVariable(name='ISEVENTEXPECTED_FINAL', sort=KSort(name='Bool')))))),)".to_string();
+    let formatted = format_k(text, Format::Advanced(Params { max_width: 100 }));
+
+    let expected = "(
+    KApply(
+        label=KLabel(
+            name='#Equals', params=(KSort(name='Bool'), KSort(name='GeneratedTopCell'))
+        ), 
+        args=(
+            KToken(token='true', sort=KSort(name='Bool')), 
+            KApply(
+                label=KLabel(
+                    name='foundry_success', params=()
+                ), 
+                args=(
+                    KVariable(
+                        name='STATUSCODE_FINAL', sort=KSort(name='StatusCode'))
+                    , 
+                    KApply(
+                        label=KLabel(
+                            name='#lookup(_,_)_EVM-TYPES_Int_Map_Int', params=()
+                        ), 
+                        args=(
+                            KVariable(name='CHEATCODE_STORAGE_FINAL', sort=KSort(name='Map')), 
+                            KToken(
+                                token='46308022326495007027972728677917914892729792999299745830475596687180801507328', 
+                                sort=KSort(name='Int')
+                            )
+                        )
+                    ), 
+                    KVariable(name='ISREVERTEXPECTED_FINAL', sort=KSort(name='Bool')), 
+                    KVariable(name='ISOPCODEEXPECTED_FINAL', sort=KSort(name='Bool')), 
+                    KVariable(name='RECORDEVENT_FINAL', sort=KSort(name='Bool')), 
+                    KVariable(name='ISEVENTEXPECTED_FINAL', sort=KSort(name='Bool'))
+                ))
+        )
+    ),
+)"
+    .to_string();
+
+    assert_eq!(formatted, expected);
+}
